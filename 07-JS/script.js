1743b28d9e1580a843d6ff03d7c402c88a0bf00a
Mariana Torres <marith.3289@gmail.com>
2024-10-28 10:18:33 -0600

    Tipos de variables

---
+++ 07-JS/script.js
@@ -0,0 +1,13 @@
+/* Tipos de datos y variables
+
+    Variable: es un espacio en memoria dentro de nuestro programa
+
+    Formas de declarar una variable: var, let y const
+
+    var: tiene un alcance global
+    let: tiene un alcance local
+    const: es una constante
+
+    scope: alcance de mis variables
+
+ */
