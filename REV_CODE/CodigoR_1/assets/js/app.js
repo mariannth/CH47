84893b9abf646e3f0c3df21dbe8aed1203cc861b
Mariana Torres <marith.3289@gmail.com>
2024-11-29 11:13:54 -0600

    Añadiendo correcciones en JS y HTML del ejercicio 1

--- REV_CODE/CodigoR_1/assets/js/app.js
+++ REV_CODE/CodigoR_1/assets/js/app.js
@@ -1,22 +1,34 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector(".name"); // Corregido, le faltaba el .
+const $b = document.querySelector(".blog"); // Corregido, tenía un # en vez del .
+const $l = document.querySelector(".location");
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) {
+  //Faltaba la palabra async
+  try {
+    $n.textContent = "cargando...";
+    const response = await fetch(`${usersEndpoint}/${username}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } // Corregido, faltaba esta condición
+    const data = await response.json(); //Faltaba la extracción de la data
+    console.log(data);
+    // Usamos la data correctamente, faltaba || para evitar undefined
+    $n.textContent = `${data.name || "Sin nombre"}`;
+    $b.textContent = `${data.blog || "Sin blog"}`;
+    $l.textContent = `${data.location || "Sin ubicación"}`;
+  } catch (error) {
+    handleError(error); // Delegamos el manejo de errores, faltaba el try catch
+  }
 }
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.error("OH NO!");
+  console.error(err);
+  $n.textContent = `Algo salió mal: ${err.message}`;  //Faltaba el $ en la variable
 }
 
-displayUser('stolinski').catch(handleError);
+// Llamamos a la función
+displayUser("stolinski").catch(handleError);
